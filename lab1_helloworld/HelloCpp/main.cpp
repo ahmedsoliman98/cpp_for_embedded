bdde0d8e828af1e96ff88026d2550ef86f15091f
Ahmed Soliman <ahmed.muhammed.soliman@gmail.com>
2021-12-21 20:42:34 +0200

    first project print my name

---
+++ lab1_helloworld/HelloCpp/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include"printName.h"
+
+using namespace std;
+
+int main()
+{
+    printName();
+}
---
+++ lab1_helloworld/HelloCpp/printName.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+
+
+void printName()
+{
+    std::cout<<"Ahmed Muhammed Soliman"<<std::endl;
+}
